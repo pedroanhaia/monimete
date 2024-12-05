49fa3224dcaf937add6a94397c6cfebd111bc57d
Gabriela <gabrielamachado078@gmail.com>
2024-12-05 14:47:47 -0300

    Teste Button

--- templates/Cities/add.php
+++ templates/Cities/add.php
@@ -26,6 +26,7 @@
             </fieldset>
             <?= $this->Form->button(__('Submit')) ?>
             <?= $this->Form->end() ?>
+            <button type="button" onclick="alert('Hello world!')">Click Me!</button>
         </div>
     </div>
 </div>
