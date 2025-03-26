596cb513cc8e0ac143cc466b4ae45fd82affef3e
Enzo-Tielet <enzotielet@gmail.com>
2025-03-26 14:06:12 -0300

    sidebar

--- templates/layout/default.php
+++ templates/layout/default.php
@@ -34,26 +34,172 @@ $cakeDescription = 'Monimete: monitoramente metereológico Agrocity';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <img src="/img/AGROCITY_LIVING_LABicon.jpg" alt="Agrocity Logo" width="100em">
-            <?=$this->Html->image('AGROCITY_LIVING_LABicon.jpg', ['alt' => 'Agrocity Logo', 'width'=>'100em'])?>
+<style>
+    
+        .flex-parent-element {
+        display: flex;
+        width: 100vw;
+        position: relative;
+
+        }
+        .main{
+            display: block;
+            width: 100%;
+            z-index:1;
+            transition: width 0.3s ease-in-out, margin-left 0.3s ease-in-out;
+        }
+        .main.stretch{
+            width: calc(300vw); 
+            margin-left: -250px; 
+        }
+        .flex-child-element {
+        flex: 1;
+        border: 2px solid blueviolet;
+        margin: 10px;
+        }
+
+        .flex-child-element:first-child {
+        margin-right: 20px;
+        z-index:2;
+        }
+        body {
+            display: flex;
+        }
+        .sidebar {
+            position: sticky;
+            top: 0;
+            height: 100vh; /* Altura da tela inteira */
+            width: 250px; /* Largura fixa */
+            background-color: #343a40; /* Cor de fundo */
+            color: white; /* Cor do texto */
+            transition: transform 0.3s ease-in-out;
+        }
+        .sidebar.hidden{
+            transform: translateX(-250px);
             
-            <a  href="<?= $this->Url->build('/') ?>"><span>Moni</span>mete</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentação</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-            <?= $this->Html->link(__('Acesso restrito'), ['controller' => 'users'],['action' => 'login'], ['class' => 'button float-right']) ?>
-        </div>
-    </nav>
-    <main class="main">
+        }
+
+        .sidebar a {
+            color: white;
+            text-decoration: none;
+            padding: 10px 15px;
+            display: block;
+        }
+
+        .sidebar a:hover {
+            background-color: #495057; /* Cor ao passar o mouse */
+        }
+        .top-nav{
+            max-width: 100vw;
+            color: white;
+            padding: 10px;
+            width: 100%;
+            position: relative;
+            top: 0;
+            left: 0;
+            z-index: 1000;
+            transition: width: 0.3s ease-in-out;
+        }
+        
+        .sidebar-button{
+            position: absolute;
+            
+            right: -45px;
+            display: flex;
+            justify-content: flex-end;
+            padding: 10px;
+            z-index: 999;
+        }
+
+        </style>
+</head>
+<!-- Sidebar -->
+
+
+<div class = "flex-parent-element"overflow-x= "auto">
+    <div style="display: flex;z-index: 2;">
+        <div class="flex_child_element"> 
+            <?php
+                $currenttemplate = $this->getRequest()->getParam('controller');
+                $currentaction = $this->getRequest()->getParam('action');
+                if(!($currenttemplate == 'Pages' && $currentaction == 'display')){
+
+            ?>
+            <div class="sidebar" id="sidebar">
+                <div class="sidebar-button" >
+                    <button id="sidebarCollapse">☰</button>
+                </div>
+                <nav>
+                    <?php
+                        
+                        $menuItems = [
+                            'Início' => ['controller' => 'Pages', 'action' => 'display', 'indexhome'],
+                            'Cidades' => ['controller' => 'Cities', 'action' => 'index'],
+                            'Metereologico' => ['controller' => 'DataMetereological', 'action' => 'index'],
+                            'Satélite' => ['controller' => 'DataSatellite', 'action' => 'index'],   
+                            'Dispositivos' => ['controller' => 'Devices', 'action' => 'index'],
+                            'Locais' => ['controller' => 'Locations', 'action' => 'index'],
+                            'Logs' => ['controller' => 'Logs', 'action' => 'index'],
+                            'Plataformas' => ['controller' => 'Platforms', 'action' => 'index'],
+                            'Serviços' => ['controller' => 'Services', 'action' => 'index'],
+                            'Usuários' => ['controller' => 'Users', 'action' => 'index'],
+                            'Configurações' => ['controller' => 'Settings', 'action' => 'index'],
+                        ];
+                        foreach ($menuItems as $label => $url) {
+                    
+                            echo $this->Html->link($label, $url, ['class' => 'side-nav-item']);
+                        }
+                    
+                    
+                    
+                    ?>
+                </nav>
+            </div>
+            <?php } ?>
+        </div>  
+        
+    </div>
+    
+    <main class="main" id="main" overflow-x: visible;>
+        <nav class="top-nav" id="top-nav">
+            <div class="top-nav-links">
+                <a target="_blank" rel="noopener" href="https://book.cakephp.org/5/">Documentação</a>
+                <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
+                <?= $this->Html->link(__('Acesso restrito'), ['controller' => 'users'],['action' => 'login'], ['class' => 'button float-right']) ?>
+            </div>
+            <div class="top-nav-title">
+                
+                <?=$this->Html->image('AGROCITY_LIVING_LABicon.jpg', ['alt' => 'Agrocity Logo', 'width'=>'100em'])?>
+                
+                <a  href="<?= $this->Url->build('/') ?>"><span>Moni</span>mete</a>
+            </div>
+        </nav>
+        
         <div class="container">
+            
             <?= $this->Flash->render() ?>
             <?= $this->fetch('content') ?>
         </div>
     </main>
-    <footer>
-    </footer>
+</div>
+</div>
+</div>
+
+</div>
+<?php
+  $currenttemplate = $this->getRequest()->getParam('controller');
+  $currentaction = $this->getRequest()->getParam('action');
+  if(!($currenttemplate == 'Pages' && $currentaction == 'display')){
+ ?>
+    <script> 
+    document.getElementById('sidebarCollapse').addEventListener('click', function() {
+    document.getElementById('top-nav').classList.toggle('stretch');
+    document.getElementById('main').classList.toggle('stretch');
+    document.getElementById('sidebar').classList.toggle('hidden');
+    });
+    </script>
+<?php
+};
+?>
 </body>
 </html>
---
+++ vendor/cakephp/authorization/src/IdentityDecorator.php
@@ -0,0 +1,191 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         1.0.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace Authorization;
+
+use ArrayAccess;
+use Authorization\Policy\ResultInterface;
+use BadMethodCallException;
+
+/**
+ * An decorator implementing the IdentityInterface.
+ *
+ * This decorator is intended to wrap the application defined identity
+ * object and proxy attributes/methods to and 'mixin' the can() method.
+ *
+ * The decorated identity must implement ArrayAccess or be an array.
+ */
+class IdentityDecorator implements IdentityInterface
+{
+    /**
+     * Identity data
+     *
+     * @var \ArrayAccess|array
+     */
+    protected ArrayAccess|array $identity;
+
+    /**
+     * Authorization Service
+     *
+     * @var \Authorization\AuthorizationServiceInterface
+     */
+    protected AuthorizationServiceInterface $authorization;
+
+    /**
+     * Constructor
+     *
+     * @param \Authorization\AuthorizationServiceInterface $service The authorization service.
+     * @param \ArrayAccess|array $identity Identity data
+     */
+    public function __construct(AuthorizationServiceInterface $service, ArrayAccess|array $identity)
+    {
+        $this->authorization = $service;
+        $this->identity = $identity;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function can(string $action, mixed $resource): bool
+    {
+        return $this->authorization->can($this, $action, $resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function canResult(string $action, mixed $resource): ResultInterface
+    {
+        return $this->authorization->canResult($this, $action, $resource);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function applyScope(string $action, mixed $resource, mixed ...$optionalArgs): mixed
+    {
+        return $this->authorization->applyScope($this, $action, $resource, ...$optionalArgs);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getOriginalData(): ArrayAccess|array
+    {
+        if (
+            $this->identity
+            && !is_array($this->identity)
+            && method_exists($this->identity, 'getOriginalData')
+        ) {
+            return $this->identity->getOriginalData();
+        }
+
+        return $this->identity;
+    }
+
+    /**
+     * Delegate unknown methods to decorated identity.
+     *
+     * @param string $method The method being invoked.
+     * @param array $args The arguments for the method.
+     * @return mixed
+     */
+    public function __call(string $method, array $args): mixed
+    {
+        if (!is_object($this->identity)) {
+            throw new BadMethodCallException("Cannot call `{$method}`. Identity data is not an object.");
+        }
+        $call = [$this->identity, $method];
+
+        /** @phpstan-ignore callable.nonCallable */
+        return $call(...$args);
+    }
+
+    /**
+     * Delegate property access to decorated identity.
+     *
+     * @param string $property The property to read.
+     * @return mixed
+     */
+    public function __get(string $property): mixed
+    {
+        return $this->identity->{$property};
+    }
+
+    /**
+     * Delegate property isset to decorated identity.
+     *
+     * @param string $property The property to read.
+     * @return bool
+     */
+    public function __isset(string $property): bool
+    {
+        return isset($this->identity->{$property});
+    }
+
+    /**
+     * Whether a offset exists
+     *
+     * @link https://secure.php.net/manual/en/arrayaccess.offsetexists.php
+     * @param mixed $offset Offset
+     * @return bool
+     */
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->identity[$offset]);
+    }
+
+    /**
+     * Offset to retrieve
+     *
+     * @link https://secure.php.net/manual/en/arrayaccess.offsetget.php
+     * @param mixed $offset Offset
+     * @return mixed
+     */
+    public function offsetGet(mixed $offset): mixed
+    {
+        if (isset($this->identity[$offset])) {
+            return $this->identity[$offset];
+        }
+
+        return null;
+    }
+
+    /**
+     * Offset to set
+     *
+     * @link https://secure.php.net/manual/en/arrayaccess.offsetset.php
+     * @param mixed $offset The offset to assign the value to.
+     * @param mixed $value Value
+     * @return void
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        $this->identity[$offset] = $value;
+    }
+
+    /**
+     * Offset to unset
+     *
+     * @link https://secure.php.net/manual/en/arrayaccess.offsetunset.php
+     * @param mixed $offset Offset
+     * @return void
+     */
+    public function offsetUnset(mixed $offset): void
+    {
+        unset($this->identity[$offset]);
+    }
+}
